daad54f2e4908759a39c0d25c4ca0d973c4809c4
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-30 01:50:31 -0400

    Changed wdi to wdi8

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,4 @@
 class Instructor < ActiveRecord::Base
   has_many :students
+  belongs_to :wdi8
 end
--- app/models/wdi8.rb
+++ app/models/wdi8.rb
@@ -1,4 +1,4 @@
-class Wdi < ActiveRecord::Base
+class Wdi8 < ActiveRecord::Base
 has_many :students
 has_many :squads
 has_many :instructors
