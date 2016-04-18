6f206cfdfe7b83668d22635304b454d8b92d01d8
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-18 19:26:51 -0400

    added columns to migration for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -40,6 +40,6 @@ class StudentsController < ApplicationController
 
   private
   def student_params
-    params.require(:student).permit(:name, :img_url, :github_url, :portfolio_url, :project1_url, :project2_url, :project3_url, :project4_url, :quote, :squad_id, :codey_id)
+    params.require(:student).permit(:name, :img_url, :github_url, :portfolio_url, :project1_url, :project2_url, :project3_url, :project4_url, :quote, :squad_id, :codey_id, :q1, :q2, :q4, :fb, :linkedin, :email)
   end
 end
