b10002780b80815be32d3ea6964994f7c6a2710d
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-18 14:13:49 -0400

    added codey_id in params for instructors and students

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -39,6 +39,6 @@ class InstructorsController < ApplicationController
 
   private
   def instructor_params
-    params.require(:instructor).permit(:name, :img_url, :squad_name)
+    params.require(:instructor).permit(:name, :img_url, :squad_name, codey_id)
   end
 end
