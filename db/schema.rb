975e9085e75d0d27d74a6cd9352e78f2d9aa3629
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-19 19:31:50 -0400

    added devise gem and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418231512) do
+ActiveRecord::Schema.define(version: 20160419142537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20160418231512) do
     t.integer "codey_id"
   end
 
-  add_index "instructors", ["codey_id"], name: "index_instructors_on_codey_id", using: :btree
   add_index "instructors", ["squad_id"], name: "index_instructors_on_squad_id", using: :btree
 
   create_table "students", force: :cascade do |t|
@@ -57,8 +56,25 @@ ActiveRecord::Schema.define(version: 20160418231512) do
     t.string  "email"
   end
 
-  add_index "students", ["codey_id"], name: "index_students_on_codey_id", using: :btree
   add_index "students", ["instructor_id"], name: "index_students_on_instructor_id", using: :btree
   add_index "students", ["squad_id"], name: "index_students_on_squad_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
