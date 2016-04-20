967c995aabb3206911bd2b5820ad90f15fa9c080
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-20 00:26:16 -0400

    added tag controller and model

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,47 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.create!(tag_params)
+
+    redirect_to
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    @tag.update(tag_params)
+
+    redirect_to
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    redirect_to 
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+end
