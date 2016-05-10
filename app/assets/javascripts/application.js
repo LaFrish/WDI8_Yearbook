1ea9d674986d743da5321dd20d994e8a5a240bc1
YOUR FULL NAME <farishta.haider@gmail.com>
2016-05-10 02:16:15 -0400

    Made updates to gallery and user pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -296,18 +296,16 @@ $(document).ready(function(){
 // }
 // gallery();
 
-function fancybox(){
-  $(document).ready(function() {
-
-    $('.fancybox').fancybox({
-      type: 'image',
-      openEffect: 'elastic',
-      closeEffect: 'elastic',
-      helpers: {
-        title: {
-          type: 'over'
-        }
-      }
+$(document).ready(function () {
+    $('li img').on('click', function () {
+        var src = $(this).attr('src');
+        var img = '<img src="' + src + '" class="img-responsive"/>';
+        $('#myModal').modal();
+        $('#myModal').on('shown.bs.modal', function () {
+            $('#myModal .modal-body').html(img);
+        });
+        $('#myModal').on('hidden.bs.modal', function () {
+            $('#myModal .modal-body').html('');
+        });
     });
-
-gallery();
+});
