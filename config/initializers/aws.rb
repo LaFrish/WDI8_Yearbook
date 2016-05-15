11ba1652b26c6504c322c1a7883ef5b4c1402ea9
YOUR FULL NAME <farishta.haider@gmail.com>
2016-05-15 00:29:54 -0400

    updates

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-Aws.config.update({
-  region: 'us-east-1',
-  credentials: Aws::Credentials.new(ENV['AKIAJEOK6U5AD7RJAFFA'], ENV['bt1oajdcczHUp97sErfoO3xz55ufNEOiHvUatODS']),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['wdi8'])
