b9f349c306d2d1fcde0e27ff621daa11612b4ac3
YOUR FULL NAME <farishta.haider@gmail.com>
2016-05-31 16:43:29 -0400

    changed color of social icons

--- db/migrate/20160516185226_create_wdiers.rb
+++ db/migrate/20160516185226_create_wdiers.rb
@@ -10,19 +10,19 @@ class CreateWdiers < ActiveRecord::Migration
       t.string :project3_url
       t.string :quote
       t.string :squad_name
-      t.integer :teaching
-      t.integer :memory
-      t.integer :favlang
-      t.integer :wisewords
-      t.integer :tag_list
-      t.integer :q1
-      t.integer :q2
-      t.integer :q3
-      t.integer :fb
-      t.integer :linkedin
-      t.integer :email
-      t.integer :role
-      t.integer :password
+      t.string :teaching
+      t.string :memory
+      t.string :favlang
+      t.string :wisewords
+      t.string :tag_list
+      t.string :q1
+      t.string :q2
+      t.string :q3
+      t.string :fb
+      t.string :linkedin
+      t.string :email
+      t.string :role
+      t.string :password
       t.integer :squad_id, index: true, foreign_key: true
       t.integer :codey_id, index: true, foreign_key: true
       t.integer :student_id, index: true, foreign_key: true
