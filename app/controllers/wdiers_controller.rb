8dfe9b4e50e5ba6e31eb5626e910047176ea7ae4
YOUR FULL NAME <farishta.haider@gmail.com>
2016-06-01 18:39:17 -0400

    set a view for instructors and studuents

--- app/controllers/wdiers_controller.rb
+++ app/controllers/wdiers_controller.rb
@@ -2,6 +2,9 @@ class WdiersController < ApplicationController
 
   def index
     @wdiers = Wdier.all
+    @instructors = Wdier.where(role: "Instructor")
+    @students = Wdier.where(role: "Student")
+    @squads = Wdier.where(:squad_id)
   end
 
   def new
@@ -16,6 +19,10 @@ class WdiersController < ApplicationController
 
   def show
     @wdier = Wdier.find(params[:id])
+    @instructor = Wdier.where(role: "Instructor")
+    @student = Wdier.where(role: "Student")
+    @squa = Wdier.where(squad_id: @wdier.squad_id)
+    # @squad = Wdier.where(:squad_id)
     # @wdier = Wdier.where(squad_id: @wdier.squad_id).not(id: @wdier.id).order(:name)
   end
 
@@ -38,6 +45,11 @@ class WdiersController < ApplicationController
     redirect_to wdiers_path(@wdier)
   end
 
+  def photos
+    @wdier = @wdier.photos.find(params[:id])
+  end
+
+  private
   def wdier_params
     params.require(:wdier).permit(:name, :img_url, :squad_name, :squad_id, :quote, :teaching, :memory, :favlang, :wisewords, :tag_list, :github_url, :portfolio_url, :project1_url, :project2_url, :project3_url, :quote, :q1, :q2, :q3, :fb, :linkedin, :email, :role,  :student_id, :instructor_id)
   end
