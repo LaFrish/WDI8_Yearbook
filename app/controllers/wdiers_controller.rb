58d5da2a7075ce103ed7d4179ab11bb628cb049a
YOUR FULL NAME <farishta.haider@gmail.com>
2016-06-05 21:42:37 -0400

    cleaned out duplicative data in schema

--- app/controllers/wdiers_controller.rb
+++ app/controllers/wdiers_controller.rb
@@ -5,7 +5,6 @@ class WdiersController < ApplicationController
     @wdiers = Wdier.all
     @instructors = Wdier.where(role: "Instructor")
     @students = Wdier.where(role: "Student")
-    @squads = Wdier.where(:squad_id)
   end
 
   def new
@@ -23,8 +22,7 @@ class WdiersController < ApplicationController
     # @instructor = Wdier.find.where(role: "Instructor")
     # @student = Wdier.where(role: "Student")
     # @squad = Wdier.where(squad_id: @wdier.squad_id)
-    # @squad = Wdier.where(:squad_id)
-    # @wdier = Wdier.where(squad_id: @wdier.squad_id).not(id: @wdier.id).order(:name)
+
   end
 
 
@@ -61,6 +59,6 @@ class WdiersController < ApplicationController
 
   private
   def wdier_params
-    params.require(:wdier).permit(:name, :img_url, :squad_name, :squad_id, :quote, :teaching, :memory, :favlang, :wisewords, :tag_list, :github_url, :portfolio_url, :project1_url, :project2_url, :project3_url, :quote, :q1, :q2, :q3, :fb, :linkedin, :email, :role,  :student_id, :instructor_id)
+    params.require(:wdier).permit(:name, :img_url, :quote, :teaching, :memory, :favlang, :wisewords, :tag_list, :github_url, :portfolio_url, :project1_url, :project2_url, :project3_url, :project4_url, :quote, :q1, :q2, :q3, :fb, :linkedin, :email, :role, :squad_name, :squad_img)
   end
 end
